5eef9cea2426dbb349840f20dad2861c003a08b7
novakgyorgy <tabbyfredplays@gmail.com>
2026-03-09 00:39:22 +0100

    init

--- tabby/app/_layout.tsx
+++ tabby/app/_layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
